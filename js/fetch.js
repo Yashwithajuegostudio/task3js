17a746bc15ad39a59325b27b2b895c0ad5f2e008
Yashwithajuegostudio <103107632+Yashwithajuegostudio@users.noreply.github.com>
2022-05-09 12:58:19 +0530

    FEAT:two pop up window added

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,6 @@ const base_api_url = "http://restapi.adequateshop.com/api/Tourist";
 
 async function getapi(url) {
   const response = await fetch(url);
-
   var data = await response.json();
   console.log(data.data);
   if (response) {
@@ -11,33 +10,34 @@ async function getapi(url) {
   show(data.data);
 }
 getapi(base_api_url);
-// getapi(api_url);
+
 function show(data) {
   var Container = document.getElementById("tourists");
   const overlay = document.getElementById("overlay");
   const modal = document.getElementById("modal");
   const modalBody = document.getElementById("modal-body");
   const closeModalButtons = document.getElementById("close-button");
+  const submit= document.getElementsByClassName('submitButton');
+  const deleteButton=document.getElementById('delete')
 
   for (var i = 0; i < data.length; i++) {
     const card = document.createElement("div");
+    card.className="card";
     const img = document.createElement("img");
-    card.style.background = "pink";
-    card.style.padding = "20px";
-    card.style.margin = "15px";
-    card.style.flexWrap = "wrap";
-    card.style.borderRadius = "20px";
-    card.style.display = "flex";
-
     img.src = data[i].tourist_profilepicture;
-    card.appendChild(img);
     const info = document.createElement("div");
-
-    info.style.paddingLeft = "50px";
-    info.style.marginTop = "30px";
-    info.style.lineHeight = "30px";
-    info.style.fontSize = "17px";
-
+    info.className="info";
+    const edit =document.createElement('button');
+    edit.className='editButton';
+    edit.innerHTML='Edit';
+    // const deleteButton=document.createElement('button');
+    // deleteButton.className='deleteButton';
+    // deleteButton.innerHTML='Delete';
+    const textarea=document.createElement('textarea');
+    textarea.className='textbox';
+    textarea.rows='4';
+   
+    console.log(data[i].id)
     info.innerHTML =
       "Name: " +
       data[i].tourist_name +
@@ -49,11 +49,52 @@ function show(data) {
       "<br> " +
       "Loaction: " +
       data[i].tourist_location;
-
+    card.appendChild(img);
     card.appendChild(info);
+    card.appendChild(edit);
+    card.appendChild(deleteButton);
+    edit.addEventListener('click',()=>{
+      if (modal==null) return;
+      console.log('editbutton clicked')
+      modal.classList.add("active");
+      overlay.classList.add("active");
+      deleteButton.classList.remove('active');
 
-    card.addEventListener("click", () => {
+     textarea.innerHTML=info.innerText;
+     modalBody.appendChild(textarea);
+     modalBody.appendChild(submit)
+
+     closeModalButtons.addEventListener("click", () => {
       if (modal == null) return;
+      modal.classList.remove("active");
+      overlay.classList.remove("active");
+     
+    });
+      
+
+    })
+    deleteButton.addEventListener('click',()=>{
+      console.log('deletebutton clicked')
+     
+    
+      modal.classList.add("active");
+      overlay.classList.add("active");
+      deleteButton.classList.add('active');
+    
+      modalBody.innerHTML = info.innerHTML;
+     
+
+     closeModalButtons.addEventListener("click", () => {
+      if (modal == null) return;
+      modal.classList.remove("active");
+      overlay.classList.remove("active");
+     
+    });
+      
+
+    })
+    card.addEventListener("click", () => {
+      if (modal!=null) return;
       modal.classList.add("active");
       overlay.classList.add("active");
 
@@ -66,6 +107,7 @@ function show(data) {
         if (modal == null) return;
         modal.classList.remove("active");
         overlay.classList.remove("active");
+       
       });
     });
     Container.appendChild(card);
